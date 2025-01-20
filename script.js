326fd5f255c21aa40240c4c3dcadba4d7ca365bf
vinayak-gr <vinayak7259@gmail.com>
2025-01-20 20:43:37 +0530

    Added Subnavbar and image slider

---
+++ script.js
@@ -0,0 +1,33 @@
+const imgs = document.querySelectorAll(".header-slider ul img");
+const controlPrev = document.querySelector(".control-prev");
+const controlNext = document.querySelector(".control-next");
+
+let n = 0;
+
+function changeSlide(){
+    for (let i = 0; i < imgs.length; i++){
+        imgs[i].style.display = "none";
+    }
+    imgs[n].style.display = "block";
+}
+changeSlide();
+
+controlPrev.addEventListener("click", (e)=>{
+    if(n > 0){
+        n--;
+    }
+    else{
+        n = imgs.length - 1;
+    }
+    changeSlide();
+})
+
+controlNext.addEventListener("click", (e)=>{
+    if(n < imgs.length - 1){
+        n++;
+    }
+    else{
+        n = 0;
+    }
+    changeSlide();
+})
