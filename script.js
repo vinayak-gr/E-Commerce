4bc3bdded361d50ec056c27d611ce83e5674c43a
vinayak-gr <vinayak7259@gmail.com>
2025-01-25 13:12:45 +0530

    Added Back to top button

--- script.js
+++ script.js
@@ -40,3 +40,19 @@ for (const product of products){
     })
 }
 
+const backToTop = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 100){
+        backToTop.style.display = "block";
+    }else{
+        backToTop.style.display = "none"
+    }
+})
+
+backToTop.addEventListener("click", ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "instant"
+    })
+})
